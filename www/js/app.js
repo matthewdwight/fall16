fdc1193a84505564534acfe479fe2989fba672ff
matthewdwight <matthewdwight@seattleacademy.org>
2015-10-08 08:50:36 -0700

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+var deviceReadyDeferred = $.Deferred();
+var jqmReadyDeferred = $.Deferred();
+
+$(document).on("deviceready", function() {
+  deviceReadyDeferred.resolve();
+});
+
+$(document).on("mobileinit", function () {
+  jqmReadyDeferred.resolve();
+});
+
+$.when(deviceReadyDeferred, jqmReadyDeferred).then(init);
+
+function init() {
+	$("testme").html ("Matthew Is Awesome");
+	setInterval(countup, 3000);
+}
+var myvalue = 3;
+function countup (){
+	myvalue = myvalue*1 + 1;
+	$("testme").html (myvalue);
+	navigator.compass.getCurrentHeading(onSuccess,onError)
+}	
+
+
+function onSuccess(heading){
+	$("mycompass").html(heading);
+}
+
+function onError(error){
+	$("mycompass").html(error);
+}
